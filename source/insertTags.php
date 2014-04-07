a5e1710dfdb844a5201d1c7c6431fb1995791110
Dominik Hofmann <dominik@ideenfrische.de>
2014-04-07 12:44:29 +0200

    add source

---
+++ source/insertTags.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('workflows.php');
+$w = new Workflows();
+
+$insertTags = file_get_contents("insertTags.csv");
+
+$fp = fopen('insertTags.csv', 'r');
+$results = array();
+$i=0;
+while (!feof($fp)) {
+    $row = fgetcsv($fp,0,';');
+    
+    
+    $temp = array(
+  		'uid' => $i,
+  		'arg' => $row[0],
+  		'title' => $row[0],
+  		'subtitle' => $row[1],
+  		'icon' => 'icon.png',
+  		'valid' => 'yes',
+  		'autocomplete' => 'autocomplete'
+  	);
+    
+    array_push( $results, $temp );
+    $i++;
+}
+fclose($fp);
+
+if(!empty($in)){
+  foreach ($results as $key => $value){
+  	if(strpos(strtolower($value['title']),strtolower($in)) === false && strpos(strtolower($value['subtitle']),strtolower($in)) === false) {
+  		unset($results[$key]);
+  	}
+  } 
+}
+echo $w->toxml( $results );
+
+?>
