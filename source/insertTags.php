85e3a008845c73f972fb6abc5c1a31a727e3887d
Dominik Hofmann <dominik@ideenfrische.de>
2014-04-07 13:05:51 +0200

    load informations remote

--- source/insertTags.php
+++ source/insertTags.php
@@ -2,16 +2,11 @@
 
 require_once('workflows.php');
 $w = new Workflows();
-
-$insertTags = file_get_contents("insertTags.csv");
-
-$fp = fopen('insertTags.csv', 'r');
+$fp = fopen('http://files.domi-online.de/insertTags.csv', 'r');
 $results = array();
 $i=0;
 while (!feof($fp)) {
     $row = fgetcsv($fp,0,';');
-    
-    
     $temp = array(
   		'uid' => $i,
   		'arg' => $row[0],
