f30422596c5b53649cdfba13cd003c09299d606f
Dominik Hofmann <dominik@ideenfrische.de>
2016-01-27 21:08:23 +0100

    get insert tags from local source

--- source/insertTags.php
+++ source/insertTags.php
@@ -2,7 +2,7 @@
 
 require_once('workflows.php');
 $w = new Workflows();
-$fp = fopen('http://files.domi1244.de/insertTags-3afe5bizOD.csv', 'r');
+$fp = fopen('insertTags.csv', 'r');
 $results = array();
 $i=0;
 while (!feof($fp)) {
@@ -16,7 +16,7 @@ while (!feof($fp)) {
   		'valid' => 'yes',
   		'autocomplete' => 'autocomplete'
   	);
-    
+
     array_push( $results, $temp );
     $i++;
 }
@@ -27,7 +27,7 @@ if(!empty($in)){
   	if(strpos(strtolower($value['title']),strtolower($in)) === false && strpos(strtolower($value['subtitle']),strtolower($in)) === false) {
   		unset($results[$key]);
   	}
-  } 
+  }
 }
 echo $w->toxml( $results );
 
